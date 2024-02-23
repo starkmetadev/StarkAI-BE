9f78ca15207b4f335c5f52c35cf682ae532fca8f
Shadow001017 <ryanniit0x902@gmail.com>
2024-02-23 06:00:47 -0500

    update api

--- routes/getImages.js
+++ routes/getImages.js
@@ -7,9 +7,13 @@ router.post("/", async (req, res) => {
   const email = req.body.email;
   try {
     const images = await Image.find({ owner: email });
-    const likeImages = await LikeImage.find({email: email});
-    const likeImageIds = likeImages.map((image)=>{return image.imageID});
-    res.json({images: images, likeImageIds: likeImageIds});
+    const likeImages = await LikeImage.find({ email: email });
+    const likeImageIds = likeImages.map((image) => {
+      return image.imageID;
+    });
+    const reversedImages = [...images].reverse();
+    const reversedLikeImageIds = [...likeImageIds].reverse();
+    res.json({ images: reversedImages, likeImageIds: reversedLikeImageIds });
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Fail" });
